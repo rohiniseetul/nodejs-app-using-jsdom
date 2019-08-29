fb0a35eaffe45ce34ed3de538ae08d365af21e80
Rohini Seetul <rohini.seetul@fenergo.com>
2019-08-29 11:33:13 +0100

    Minor updates

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+console.log(
+  "This is a node.js app using the latest version of jsdom to prove that 'onBlur' event is being fired incorrectly!"
+);
+
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
@@ -5,44 +9,57 @@ const { document } = new JSDOM(`
                 <input 
                     type="text" 
                     id="foo" 
-                    onfocus="() => console.log('foo - onfocus event handler is fired')" 
-                    onblur="() => console.log('foo - onblur event handler is fired')"  />
+                    onfocus="() => console.log('foo input field - onfocus event handler is fired')" 
+                    onblur="() => console.log('foo input field - onblur event handler is fired')"  />
 
                 <input 
                     type="text" 
                     id="bar" 
-                    onfocus="() => console.log('bar - onfocus event handler is fired')" 
-                    onblur="() => console.log('bar - onblur event handler is fired')"  />
+                    onfocus="() => console.log('bar input field - onfocus event handler is fired')" 
+                    onblur="() => console.log('bar input field - onblur event handler is fired')"  />
 `).window;
-
-console.log("This is a node.js app");
 
 getFooInputElement = () => document.getElementById("foo");
 getBarInputElement = () => document.getElementById("bar");
 
+// Attached event listeners
 getFooInputElement().addEventListener(
   "focus",
-  () => console.log("foo - heard browser focus event"),
+  () => console.log("foo input field - heard browser focus event"),
   { capture: true }
 );
 getFooInputElement().addEventListener(
   "blur",
-  () => console.log("foo - heard browser blur event"),
+  () => console.log("foo input field - heard browser blur event"),
   { capture: true }
 );
 
 getBarInputElement().addEventListener(
   "focus",
-  () => console.log("bar - heard browser focus event"),
+  () => console.log("bar input field - heard browser focus event"),
   { capture: true }
 );
 getBarInputElement().addEventListener(
   "blur",
-  () => console.log("bar - heard browser blur event"),
+  () => console.log("bar input field - heard browser blur event"),
   { capture: true }
 );
 
 // Execute
-console.log("Running.........");
+console.log(
+  "App runing...\n-------------------------------------------------------------------------"
+);
+console.log("Calling 'focus()' on an input field");
 getFooInputElement().focus();
+
+console.log(
+  "\nCalling on focus on the same foo input field which has focused already"
+);
 getFooInputElement().focus();
+
+console.log(
+  "-------------------------------------------------------------------------"
+);
+console.log(
+  "Result/Issue: As we can see, `onBlur` event is fired when we call 'focus()' on the focused foo input field. "
+);
